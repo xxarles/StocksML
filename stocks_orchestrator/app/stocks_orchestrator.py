566471d419d6a3b441204d2195e2fb2599ccbde1
Thales Sinelli <thalessinelli@gmail.com>
2024-10-27 23:44:33 -0300

    Add additional call to the orchestrator

--- stocks_orchestrator/app/stocks_orchestrator.py
+++ stocks_orchestrator/app/stocks_orchestrator.py
@@ -1,5 +1,6 @@
 import time
 import logging
+from urllib import response
 import requests
 import os
 
@@ -20,6 +21,22 @@ def get_module_logger(mod_name):
 logger = get_module_logger(__file__)
 
 
+def register_new_ingestions():
+    response = requests.get(os.environ.get("REGISTER_NEW_INGESTIONS_URL", ""))
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        logger.error(e)
+
+
+def cleanup_stale_ingestion():
+    response = requests.get(os.environ.get("CLEANUP_INGESTION_PENDING_STATUS", ""))
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        logger.error(e)
+
+
 def start_next_ingestion():
     response = requests.get(os.environ.get("NEXT_INGESTION_URL", ""))
     try:
@@ -38,12 +55,18 @@ def start_next_ingestion():
 if __name__ == "__main__":
     while True:
         try:
+            logger.info("Cleaning up stale ingestion")
+            cleanup_stale_ingestion()
+
+            logger.info("Checking to register new ingestions")
+            register_new_ingestions()
+
             logger.info("Checking for new ingestions.")
             start_next_ingestion()
 
             logger.info("Sleeping for 10 seconds before next loop")
-            time.sleep(10)
+            time.sleep(2)
 
         except Exception as e:
             logger.error(e)
-            time.sleep(10)
+            time.sleep(2)
